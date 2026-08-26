ded204318e9df388113c9a50ef844da30684fba3
Blisto91 <Blisto@protonmail.com>
2026-08-26 21:11:38 +0200

    Check env var at runtime

--- main.rs
+++ main.rs
@@ -6,6 +6,6 @@
 use std::env;
   
 fn main() {
-    let path = env!("GITHUB_ACTION_PATH");
+    let path = env::var("GITHUB_ACTION_PATH").unwrap();
     println!("::add-matcher::{}/gcc_matcher.json", path);
 }
