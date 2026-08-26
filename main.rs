8185d745b26709f20d29c969c7824b43c0eb12d0
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2026-08-26 12:57:34 +0200

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S cargo +nightly -Zscript
+---cargo
+[dependencies]
+---
+
+use std::env;
+  
+fn main() {
+    let path = env::current_dir().unwrap().into_string().unwrap();
+    println!("::add-matcher::{}gcc_matcher.json", path);
+}
