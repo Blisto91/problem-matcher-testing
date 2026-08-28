542ecf9a9d5b4f027553b581e66df4e11e6cbf36
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2026-08-28 07:06:43 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -10,11 +10,11 @@ fn main() {
     let skip_dirs = env::args().nth(1).expect("no skip-directories argument given");
     let path = env::var("GITHUB_ACTION_PATH").unwrap();
     
-    let gcc_matcher = fs::read_to_string("gcc_matcher.json").unwrap();
-    gcc_matcher.replace("{{SKIP_DIRS}}", &skip_dirs);
-    fs:: write("gcc_matcher.json", gcc_matcher);
+    let old_gcc_matcher = fs::read_to_string("gcc_matcher.json").unwrap();
+    let new_gcc_matcher = gcc_matcher.replace("{{SKIP_DIRS}}", &skip_dirs);
+    fs::write("gcc_matcher.json", new_gcc_matcher).unwrap();
 
-    println!("::add-matcher::{}/gcc_matcher.json", path);
+    println!("::add-matcher::{}/gcc_matcher.json", path).unwrap();
 }
 
 fn escape_chars (s: &str) -> String {
