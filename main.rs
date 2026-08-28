bc5dc845fe1ca6b7167fb012b767b4c940af337b
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2026-08-28 07:40:42 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,19 +5,22 @@ edition = "2024"
 
 use std::env;
 use std::fs;
-  
+
 fn main() {
-    let skip_dirs = env::args().nth(1).expect("no skip-directories argument given");
-    let path = env::var("GITHUB_ACTION_PATH").unwrap();
-    
-    let old_gcc_matcher = fs::read_to_string("gcc_matcher.json").unwrap();
+    let skip_dirs = env::args()
+        .nth(1)
+        .expect("no skip-directories argument given");
+    let mut matcher_path = env::var("GITHUB_ACTION_PATH").unwrap();
+    matcher_path.push_str("gcc_matcher.json");
+
+    let old_gcc_matcher = fs::read_to_string(&matcher_path).unwrap();
     let new_gcc_matcher = old_gcc_matcher.replace("{{SKIP_DIRS}}", &skip_dirs);
     fs::write("gcc_matcher.json", new_gcc_matcher).unwrap();
 
-    println!("::add-matcher::{}/gcc_matcher.json", path);
+    println!("::add-matcher::{}", matcher_path);
 }
 
-fn escape_chars (s: &str) -> String {
+fn escape_chars(s: &str) -> String {
     let meta_chars = String::from(r"\+-?.*^$|()[]{}");
     let mut buffer = String::new();
 
