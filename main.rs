c6aa00333d48ddaeedbd3f88138df5c16050a217
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2026-08-28 08:25:36 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -9,7 +9,7 @@ use std::fs;
 fn main() {
     let skip_dirs = env::args().nth(1);
     let mut matcher_path = env::var("GITHUB_ACTION_PATH").unwrap();
-    matcher_path.push_str("gcc_matcher.json");
+    matcher_path.push_str("/gcc_matcher.json");
 
     let mut gcc_matcher = fs::read_to_string(&matcher_path).unwrap();
 
@@ -19,7 +19,7 @@ fn main() {
         gcc_matcher = gcc_matcher.replace("{{SKIP_DIRS}}", "//");
     }
 
-    fs::write("gcc_matcher.json", gcc_matcher).unwrap();
+    fs::write(matcher_path, gcc_matcher).unwrap();
 
     println!("::add-matcher::{}", matcher_path);
 }
